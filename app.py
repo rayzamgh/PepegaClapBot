423b2edf603ecdc5e85be9345974b79a77ec422c
rayzamgh <13517073@std.stei.itb.ac.id>
2020-08-17 19:53:47 +0700

    pepegaclap

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, request, abort
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+import sys
+import numpy as np
 
 from linebot import (
     LineBotApi, WebhookHandler
