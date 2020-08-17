b55bbb0264d8077e2c9ba2ed65902e2a0b44a0e3
rayzamgh <13517073@std.stei.itb.ac.id>
2020-08-17 20:16:11 +0700

    pepegaclap

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text="PEPEGANISM"))
 
 import os
 if __name__ == "__main__":
