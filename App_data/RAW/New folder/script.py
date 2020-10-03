6d2437a7d73b6c37e3d2408138f63eca1adae01d
rayzamgh <13517073@std.stei.itb.ac.id>
2020-10-03 21:26:18 +0700

    renamed

--- App_data/RAW/New folder/script.py
+++ App_data/RAW/New folder/script.py
@@ -1,14 +1,31 @@
 import os 
+import pandas as pd  
 
 arr = os.listdir()
 
 #"16519039"
 
+data = pd.read_csv("nim.csv") 
+data.rename(columns={"NIM": "nimbaru", "NIM TPB": "nimlama"}, inplace = True)
+print(data.head())
+
+nimbaru = data['nimbaru'].tolist()
+nimlama = data['nimlama'].tolist()
+
+res = dict(zip(nimlama, nimbaru)) 
+
+#print ("Resultant dictionary is : " +  str(res)) 
+
 for x in arr:
-    if not(x.startswith('BOTRDY')):
+    if (x.startswith('BOTRDY_16519')):
+        curnim = int(x[7:15])
+        print(curnim , " : ", res[curnim])
+
         if x.lower().endswith('.png'):
-            os.rename(x, "BOTRDY_" + x[:8] + '.png')
+            print(x, "BOTRDY_" + str(res[curnim]) + '.png')
+            os.rename(x, "BOTRDY_" + str(res[curnim]) + '.png')
 
         if x.lower().endswith('.jpg'):
-            os.rename(x, "BOTRDY_" + x[:8] + '.jpg')
+            print(x, "BOTRDY_" + str(res[curnim]) + '.jpg')
+            os.rename(x, "BOTRDY_" + str(res[curnim]) + '.jpg')
     
