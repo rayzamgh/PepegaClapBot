505bbfb0a389ceb645c51b7d382b2c5ddeb75c14
rayzamgh <13517073@std.stei.itb.ac.id>
2020-11-27 23:05:21 +0700

    pepega

--- app.py
+++ app.py
@@ -102,11 +102,11 @@ def handle_message(event):
         with open("static/" + "tempimg", 'wb') as fd:
             for chunk in message_content.iter_content():
                 fd.write(chunk)
-            tempfile_path = fd.name
-            print("PATHING", staticurl + tempfile_path)
+            tempfile_path = staticurl + tempimg
+            print("PATHING", tempfile_path)
             line_bot_api.reply_message(
                     event.reply_token,
-                    ImageSendMessage(staticurl + tempfile_path, staticurl + tempfile_path)
+                    ImageSendMessage(tempfile_path, tempfile_path)
                 )
         return
 
